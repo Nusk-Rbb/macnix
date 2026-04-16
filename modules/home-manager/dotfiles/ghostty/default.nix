9bc9bd68900eb8f95555eb67482a511c2a47b03c
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2026-04-16 19:46:50 +0900

    :sparkles: fix ghostty config

--- modules/home-manager/dotfiles/ghostty/default.nix
+++ modules/home-manager/dotfiles/ghostty/default.nix
@@ -4,12 +4,10 @@
   programs.ghostty-bin = {
     enable = true;
     settings = {
-      term = "xterm-256color";
+      theme = "Idle Toes";
       font-family = "FiraCode Nerd Font";
       command = "/etc/profiles/per-user/nusk/bin/fish";
-      background-opacity = 0.7;
-      window-padding-x = 10;
-      window-padding-y = 10;
+      background-opacity = 0.5;
     };
   };
 }
